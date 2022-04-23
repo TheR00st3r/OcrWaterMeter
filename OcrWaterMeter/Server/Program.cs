20730e49b746e57ef8a2031c8ba6de72c8f77e5f
Alexander Volz <Alexander@volzit.de>
2022-04-23 18:59:17 +0200

    added basic healthcheck

--- OcrWaterMeter/Server/Program.cs
+++ OcrWaterMeter/Server/Program.cs
@@ -14,7 +14,7 @@ var builder = WebApplication.CreateBuilder(new WebApplicationOptions
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 builder.Services.AddLiteDb(liteDbName);
-
+builder.Services.AddHealthChecks();
 
 builder.WebHost.UseStaticWebAssets();
 
@@ -27,6 +27,7 @@ if (app.Environment.IsDevelopment())
 }
 else
 {
+    app.MapHealthChecks("/healthz");
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     // app.UseHsts();
